cf602ff813f5fcd00a011abd871118cb720ea858
JennyGoe <jschell@gwdg.de>
2018-04-16 08:44:51 +0200

    Update racurve.r
    axis adjustment (xlim, ylim arguments) added

--- R/racurve.r
+++ R/racurve.r
@@ -20,30 +20,58 @@
 #' @author Friedemann Goral \email{fgoral@gwdg.de}
 #' @export
 
-racurve <-  function(matrix, main = "Rank-abundance diagram", ylog = FALSE) {
+racurve <-  function(matrix, main = "Rank-abundance diagram", nlab = 0, ylog = FALSE, frequency = FALSE, ylim = NULL, xlim = NULL) {
   if(!is.data.frame(matrix)) {
     matrix <- data.frame(matrix)
   }
-
-  abund <- apply(matrix, 2, sum)
+  
+  freq <- sort(apply(matrix>0, 2, sum), decreasing = T)
+  
+  abund <- sort(apply(matrix, 2, sum), decreasing = T)
   sum(abund)
-  rel.abund <- abund / sum(abund)
-
-  if(ylog == TRUE) {
-    plot(sort(rel.abund, decreasing = T), xlab="Abundance Rank", ylab="Relative abundance",
-         main=main, log="y")
-
-  } else {
-    plot(sort(rel.abund, decreasing = T), xlab="Abundance Rank", ylab="Relative abundance",
-     main=main)
+  rel.abund <- sort((abund / sum(abund)), decreasing = T)
+  
+  labels <- names(head(rel.abund, n = nlab))
+  
+  if(frequency == FALSE) {
+    
+    if(ylog == TRUE) {
+      plot(rel.abund, xlab="Abundance Rank", ylab="Relative Abundance",
+           main=main, log="y", ylim = ylim, xlim = xlim)
+      lines(rel.abund)
+      if(nlab != 0) {
+        text(head(rel.abund, n = nlab), labels = labels, pos = 4, cex = 0.7)
+      }
+      
+    } else {
+      plot(rel.abund, xlab="Abundance Rank", ylab="Relative Abundance",
+           main=main, , ylim = ylim, xlim = xlim)
+      lines(rel.abund)
+      if(nlab != 0) {
+        text(head(rel.abund, n = nlab), labels = labels, pos = 4, cex = 0.7)
+      }
+    }
+  } else if(frequency == TRUE) {
+    
+    if(ylog == TRUE) {
+      plot(freq, xlab="Frequency Rank", ylab="Frequency",
+           main=main, log="y", ylim = ylim, xlim = xlim)
+      lines(freq)
+      if(nlab != 0) {
+        text(head(freq, n = nlab), labels = labels, pos = 4, cex = 0.7)
+      }
+      
+    } else {
+      plot(freq, xlab="Frequency Rank", ylab="Frequency",
+           main=main, ylim = ylim, xlim = xlim)
+      lines(freq)
+      if(nlab != 0) {
+        text(head(freq, n = nlab), labels = labels, pos = 4, cex = 0.7)
+      }
+    }
   }
+  
+  out <- list(abund = abund, rel.abund = rel.abund, freq = freq)
+  invisible(out)
 }
-
-
-
-
-
-
-
-
 
